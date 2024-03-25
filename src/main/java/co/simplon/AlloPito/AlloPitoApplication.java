28a1c51455443f6ecafbeb1fe0de390de99a8c54
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-25 14:43:43 +0100

    build : initialize Spring Boot application

---
+++ src/main/java/co/simplon/AlloPito/AlloPitoApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.AlloPito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloPitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloPitoApplication.class, args);
+	}
+
+}
