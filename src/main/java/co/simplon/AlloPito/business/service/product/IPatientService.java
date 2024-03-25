102187f18a508a7e78361c0f8eb25ac85275d92e
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-25 16:06:57 +0100

    feat(Service): Patient Service

---
+++ src/main/java/co/simplon/AlloPito/business/service/product/IPatientService.java
@@ -0,0 +1,17 @@
+package co.simplon.AlloPito.business.service.product;
+
+import java.util.List;
+
+import co.simplon.AlloPito.business.dto.PatientDto;
+
+public interface IPatientService {
+
+	public List<PatientDto> getPatients();
+
+	public PatientDto getPatientById(final int id);
+
+	public PatientDto postPatient(final PatientDto patientDto);
+
+	public void deletePatient(final PatientDto patientDto);
+
+}
---
+++ src/main/java/co/simplon/AlloPito/business/service/product/PatientServiceImpl.java
@@ -0,0 +1,43 @@
+package co.simplon.AlloPito.business.service.product;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.simplon.AlloPito.business.convert.PatientConvert;
+import co.simplon.AlloPito.business.dto.PatientDto;
+import co.simplon.AlloPito.persistence.repository.IPatientRepository;
+
+@Service
+public class PatientServiceImpl implements IPatientService {
+
+	private IPatientRepository repo;
+
+	@Override
+	public List<PatientDto> getPatients() {
+		return PatientConvert.getInstance().convertListEntityToListDto(repo.findAll());
+	}
+
+	@Override
+	public PatientDto getPatientById(final int id) {
+		return PatientConvert.getInstance().convertEntityToDTo(repo.getReferenceById(id));
+	}
+
+	@Override
+	public PatientDto postPatient(final PatientDto patientDto) {
+		return PatientConvert.getInstance()
+				.convertEntityToDTo(repo.save(PatientConvert.getInstance().convertDtoToEntity(patientDto)));
+	}
+
+	@Override
+	public void deletePatient(final PatientDto patientDto) {
+		repo.delete(PatientConvert.getInstance().convertDtoToEntity(patientDto));
+	}
+
+	@Autowired
+	public void setRepo(final IPatientRepository repo) {
+		this.repo = repo;
+	}
+
+}
