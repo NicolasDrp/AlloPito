956b74a52d6c2ebaaa9debd31c310914af4eb619
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-26 10:16:32 +0100

    feat(Dto): Service Dto

---
+++ src/main/java/co/simplon/AlloPito/business/dto/ServiceDto.java
@@ -0,0 +1,25 @@
+package co.simplon.AlloPito.business.dto;
+
+public class ServiceDto {
+
+	private int id_service;
+
+	private String name_service;
+
+	public int getId_service() {
+		return id_service;
+	}
+
+	public void setId_service(int id_service) {
+		this.id_service = id_service;
+	}
+
+	public String getName_service() {
+		return name_service;
+	}
+
+	public void setName_service(String name_service) {
+		this.name_service = name_service;
+	}
+
+}
