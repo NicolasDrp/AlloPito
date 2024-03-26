2e9fd7824b11f1b625117c65e6a36a451d258964
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-26 09:54:53 +0100

    feat(Entity): Service Entity

---
+++ src/main/java/co/simplon/AlloPito/persistence/entity/Service.java
@@ -0,0 +1,37 @@
+package co.simplon.AlloPito.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "service")
+public class Service {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_service;
+
+	@Column(name = "name_service")
+	private String name_service;
+
+	public int getId_service() {
+		return id_service;
+	}
+
+	public void setId_service(int id_service) {
+		this.id_service = id_service;
+	}
+
+	public String getName_service() {
+		return name_service;
+	}
+
+	public void setName_service(String name_service) {
+		this.name_service = name_service;
+	}
+
+}
