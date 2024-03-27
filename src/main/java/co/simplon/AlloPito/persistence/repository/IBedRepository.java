9ee4ad483d56df9f9c2db729a291b22708ad3ecd
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-27 12:26:13 +0100

    feat(repository): removePatientFromBed

--- src/main/java/co/simplon/AlloPito/persistence/repository/IBedRepository.java
+++ src/main/java/co/simplon/AlloPito/persistence/repository/IBedRepository.java
@@ -7,11 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import co.simplon.AlloPito.persistence.entity.Bed;
+import co.simplon.AlloPito.persistence.entity.Patient;
 
 @Repository
 public interface IBedRepository extends JpaRepository<Bed, Integer> {
 
 	@Query(BedQuery.GET_EMPTY_BED_BY_SERVICE)
 	List<Bed> getEmptyBedsByService(final int idService);
+
+	@Query(BedQuery.REMOVE_PATIENT_FROM_BED)
+	List<Bed> removePatientFromBed(final Patient patient);
 
 }
