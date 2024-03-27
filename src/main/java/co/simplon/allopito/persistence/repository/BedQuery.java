a442a040dc0511a38a5c9216a6163e8ecc3aeb8e
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-27 15:24:44 +0100

    refactor(Repository): change attributes names in Query file

--- src/main/java/co/simplon/allopito/persistence/repository/BedQuery.java
+++ src/main/java/co/simplon/allopito/persistence/repository/BedQuery.java
@@ -6,7 +6,7 @@ public class BedQuery {
 		
 	}
 
-	public static final String GET_EMPTY_BED_BY_SERVICE = "SELECT b FROM Bed b JOIN Room r on b.room = r WHERE b.patient IS NULL AND r.service.id_service = :idService";
+	public static final String GET_EMPTY_BED_BY_SERVICE = "SELECT b FROM Bed b JOIN Room r on b.room = r WHERE b.patient IS NULL AND r.service.idService = :idService";
 
 	public static final String REMOVE_PATIENT_FROM_BED = "UPDATE Bed b SET b.patient = NULL WHERE b.patient = :patient";
 
