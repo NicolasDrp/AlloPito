6e13f6262a9d27d30dc01f2081a5e500098da991
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-28 12:04:48 +0100

    docs(Repository): documentation of the repositories

--- src/main/java/co/simplon/allopito/persistence/repository/IBedRepository.java
+++ src/main/java/co/simplon/allopito/persistence/repository/IBedRepository.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Repository;
 import co.simplon.allopito.persistence.entity.Bed;
 import co.simplon.allopito.persistence.entity.Patient;
 
+/**
+ * This interface represents a repository for accessing Bed entities in the
+ * system.
+ */
 @Repository
 public interface IBedRepository extends JpaRepository<Bed, Integer> {
 
--- src/main/java/co/simplon/allopito/persistence/repository/IPatientRepository.java
+++ src/main/java/co/simplon/allopito/persistence/repository/IPatientRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import co.simplon.allopito.persistence.entity.Patient;
 
+/**
+ * This interface represents a repository for accessing Patient entities in the
+ * system.
+ */
 @Repository
 public interface IPatientRepository extends JpaRepository<Patient, Integer> {
 
