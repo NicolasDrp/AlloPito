b8c783d725c185e8396f17c4c3a7b4206c93e151
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-28 13:29:42 +0100

    docs(Controller): documentation for the bed Controllers

--- src/main/java/co/simplon/allopito/presentation/controller/bed/DeleteBedController.java
+++ src/main/java/co/simplon/allopito/presentation/controller/bed/DeleteBedController.java
@@ -9,17 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 import co.simplon.allopito.business.dto.BedDto;
 import co.simplon.allopito.business.service.bed.IBedService;
 
+/**
+ * Controller class for handling requests to delete a bed.
+ */
 @RestController
 @CrossOrigin
 public class DeleteBedController {
 
 	private IBedService service;
 
+	/**
+	 * Deletes a bed.
+	 *
+	 * @param bedDto The DTO representing the bed to be deleted.
+	 */
 	@DeleteMapping("/beds")
 	public void deleteBed(@RequestBody final BedDto bedDto) {
 		service.deleteBed(bedDto);
 	}
 
+	/**
+	 * Sets the bed service for this controller.
+	 *
+	 * @param service The bed service to be set.
+	 */
 	@Autowired
 	public void setService(IBedService service) {
 		this.service = service;
--- src/main/java/co/simplon/allopito/presentation/controller/bed/GetBedByIdController.java
+++ src/main/java/co/simplon/allopito/presentation/controller/bed/GetBedByIdController.java
@@ -9,17 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 import co.simplon.allopito.business.dto.BedDto;
 import co.simplon.allopito.business.service.bed.IBedService;
 
+/**
+ * Controller class for handling requests to get a bed by its ID.
+ */
 @RestController
 @CrossOrigin
 public class GetBedByIdController {
 
 	private IBedService service;
 
+	/**
+	 * Retrieves a bed by its ID.
+	 *
+	 * @param id The ID of the bed to retrieve.
+	 * @return The DTO representing the retrieved bed.
+	 */
 	@GetMapping("/beds/{id}")
 	public BedDto getBedById(@PathVariable final int id) {
 		return service.getBedById(id);
 	}
 
+	/**
+	 * Sets the bed service for this controller.
+	 *
+	 * @param service The bed service to be set.
+	 */
 	@Autowired
 	public void setService(IBedService service) {
 		this.service = service;
