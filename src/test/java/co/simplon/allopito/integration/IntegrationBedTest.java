a28b4c031ad194666e15ebf9c1d070bfc04c34f5
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-28 22:27:05 +0100

    test(endpoints): test Bed endpoints

--- src/test/java/co/simplon/allopito/integration/IntegrationBedTest.java
+++ src/test/java/co/simplon/allopito/integration/IntegrationBedTest.java
@@ -13,16 +13,20 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import co.simplon.allopito.business.convert.RoomConvert;
 import co.simplon.allopito.business.convert.ServiceConvert;
+import co.simplon.allopito.business.dto.BedDto;
 import co.simplon.allopito.business.dto.RoomDto;
 import co.simplon.allopito.business.dto.ServiceDto;
+import co.simplon.allopito.persistence.entity.Bed;
 import co.simplon.allopito.persistence.entity.Room;
 import co.simplon.allopito.persistence.entity.Service;
+import co.simplon.allopito.persistence.repository.IBedRepository;
 import co.simplon.allopito.persistence.repository.IRoomRepository;
 import co.simplon.allopito.persistence.repository.IServiceRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class IntegrationRoomTest {
+public class IntegrationBedTest {
 
 	@LocalServerPort
 	private int port;
@@ -30,37 +34,47 @@ public class IntegrationRoomTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	private static BedDto bed = new BedDto();
+
 	private static RoomDto room = new RoomDto();
 
 	private static ServiceDto service = new ServiceDto();
 
 	@Autowired
-	private IRoomRepository repo;
+	private IBedRepository repo;
 
 	@Autowired
 	private IServiceRepository repoService;
 
+	@Autowired
+	private IRoomRepository repoRoom;
+
 	@BeforeEach
 	public void setUpTestData() {
 		repo.deleteAll();
-		repoService.deleteAll();
+		repoRoom.deleteAll();
 
 		Service service = new Service();
 		service.setNameService("service");
 		repoService.save(service);
 
 		Room room1 = new Room();
-		room1.setNumberRoom("4568");
+		room1.setNumberRoom("548");
 		room1.setService(service);
+		repoRoom.save(room1);
 
-		repo.save(room1);
+		Bed bed1 = new Bed();
+		bed1.setPatient(null);
+		bed1.setRoom(room1);
+
+		repo.save(bed1);
 	}
 
 	@Test
 	@Order(1)
-	void testGetRoomByIdEndpoint() {
+	void testGetBedByIdEndpoint() {
 		try {
-			ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/rooms/1",
+			ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/beds/1",
 					String.class);
 
 			assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -71,9 +85,9 @@ public class IntegrationRoomTest {
 
 	@Test
 	@Order(2)
-	void testGetRoomsEndpoint() {
+	void testGetBedsEndpoint() {
 		try {
-			ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/rooms",
+			ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/beds",
 					String.class);
 
 			assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -84,14 +98,16 @@ public class IntegrationRoomTest {
 
 	@Test
 	@Order(3)
-	void testPostRoomEndpoint() {
+	void testPostBedEndpoint() {
 		try {
 			service.setNameService("service");
 			Service serviceEntity = repoService.save(ServiceConvert.getInstance().convertDtoToEntity(service));
 			room.setNumberRoom("4568");
 			room.setService(ServiceConvert.getInstance().convertEntityToDto(serviceEntity));
-			ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/rooms",
-					room, String.class);
+			Room roomEntity = repoRoom.save(RoomConvert.getInstance().convertDtoToEntity(room));
+			bed.setRoom(RoomConvert.getInstance().convertEntityToDto(roomEntity));
+			ResponseEntity<String> response = this.restTemplate.postForEntity("http://localhost:" + port + "/beds", bed,
+					String.class);
 
 			assertEquals(HttpStatus.OK, response.getStatusCode());
 		} catch (Exception e) {
@@ -101,9 +117,9 @@ public class IntegrationRoomTest {
 
 	@Test
 	@Order(4)
-	void testDeleteRoomEndpoint() {
+	void testDeleteBedEndpoint() {
 		try {
-			this.restTemplate.delete("http://localhost:" + port + "/rooms", room, String.class);
+			this.restTemplate.delete("http://localhost:" + port + "/beds", bed, String.class);
 		} catch (Exception e) {
 			fail(e);
 		}
