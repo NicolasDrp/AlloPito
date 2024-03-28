e8511d226594f0526bd4932d4676372a19eddf8a
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-28 12:02:08 +0100

    docs(Entity): documentation of the entities

--- src/main/java/co/simplon/allopito/persistence/entity/Room.java
+++ src/main/java/co/simplon/allopito/persistence/entity/Room.java
@@ -9,43 +9,84 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+/**
+ * This class represents a Room entity in the system.
+ */
 @Entity
 @Table(name = "room")
 public class Room {
 
+	/**
+	 * The unique identifier of the room.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idRoom;
 
+	/**
+	 * The room number.
+	 */
 	@Column(name = "number_room", length = 5, nullable = false)
 	private String numberRoom;
 
+	/**
+	 * The service associated with the room.
+	 */
 	@ManyToOne
 	@JoinColumn(name = "id_service", nullable = false)
 	private Service service;
 
+	/**
+	 * Returns the unique identifier of the room.
+	 *
+	 * @return The unique identifier of the room.
+	 */
 	public int getIdRoom() {
 		return idRoom;
 	}
 
+	/**
+	 * Sets the unique identifier of the room.
+	 *
+	 * @param idRoom The unique identifier of the room.
+	 */
 	public void setIdRoom(int idRoom) {
 		this.idRoom = idRoom;
 	}
 
+	/**
+	 * Returns the room number.
+	 *
+	 * @return The room number.
+	 */
 	public String getNumberRoom() {
 		return numberRoom;
 	}
 
+	/**
+	 * Sets the room number.
+	 *
+	 * @param numberRoom The room number.
+	 */
 	public void setNumberRoom(String numberRoom) {
 		this.numberRoom = numberRoom;
 	}
 
+	/**
+	 * Returns the service associated with the room.
+	 *
+	 * @return The service associated with the room.
+	 */
 	public Service getService() {
 		return service;
 	}
 
+	/**
+	 * Sets the service associated with the room.
+	 *
+	 * @param service The service associated with the room.
+	 */
 	public void setService(Service service) {
 		this.service = service;
 	}
-
 }
--- src/main/java/co/simplon/allopito/persistence/entity/Service.java
+++ src/main/java/co/simplon/allopito/persistence/entity/Service.java
@@ -7,31 +7,59 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+/**
+ * This class represents a Service entity in the system.
+ */
 @Entity
 @Table(name = "service")
 public class Service {
 
+	/**
+	 * The unique identifier of the service.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idService;
 
+	/**
+	 * The name of the service.
+	 */
 	@Column(name = "name_service", length = 50, nullable = false)
 	private String nameService;
 
+	/**
+	 * Returns the unique identifier of the service.
+	 *
+	 * @return The unique identifier of the service.
+	 */
 	public int getIdService() {
 		return idService;
 	}
 
+	/**
+	 * Sets the unique identifier of the service.
+	 *
+	 * @param idService The unique identifier of the service.
+	 */
 	public void setIdService(int idService) {
 		this.idService = idService;
 	}
 
+	/**
+	 * Returns the name of the service.
+	 *
+	 * @return The name of the service.
+	 */
 	public String getNameService() {
 		return nameService;
 	}
 
+	/**
+	 * Sets the name of the service.
+	 *
+	 * @param nameService The name of the service.
+	 */
 	public void setNameService(String nameService) {
 		this.nameService = nameService;
 	}
-
 }
