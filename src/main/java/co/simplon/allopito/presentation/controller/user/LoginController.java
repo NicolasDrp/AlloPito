7f9f696ad792a2c28307822ce09660af9b12c809
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-28 23:04:05 +0100

    feat(Security): add configuration Spring security

---
+++ src/main/java/co/simplon/allopito/presentation/controller/user/LoginController.java
@@ -0,0 +1,23 @@
+package co.simplon.allopito.presentation.controller.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.allopito.presentation.controller.service.JWTService;
+
+@RestController
+public class LoginController {
+
+	JWTService jwtService;
+
+	public LoginController(JWTService jwtService) {
+		this.jwtService = jwtService;
+	}
+
+	@PostMapping("/login")
+	public String getToken(Authentication authentication) {
+		return jwtService.generateToken(authentication);
+	}
+
+}
