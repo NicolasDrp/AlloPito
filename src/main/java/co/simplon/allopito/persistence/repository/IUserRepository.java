cff38fd7d469b74537eb667062967cef66573384
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-29 10:01:26 +0100

    feat(Repository): User Repository

---
+++ src/main/java/co/simplon/allopito/persistence/repository/IUserRepository.java
@@ -0,0 +1,14 @@
+package co.simplon.allopito.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.simplon.allopito.persistence.entity.User;
+
+/**
+ * This interface represents a repository for accessing User entities in the
+ * system.
+ */
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+}
